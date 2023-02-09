b6e645f760d302ea1cf2057497f34f57923932f7
gulaa <gula@pr.km.ua>
2023-02-09 17:35:06 +0200

    add basic weather template

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 from flask import Flask, render_template
+import pandas as pd
 
 app = Flask('Website')
 
 @app.route('/')
 def home():
-    return render_template('tutorial.html')
+    return render_template('home.html')
+
+@app.route('/api/<station>/<date>')
+def about(station, date):
+    # df = pd.read_csv('data.csv')
+    # temperature = df.station(date)
+    temperature = 23
+    return str(temperature)
 
 app.run(debug=True)
